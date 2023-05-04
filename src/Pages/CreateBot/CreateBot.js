164ce0cbeee8b40b1b49c421bddfb78057945b78
Vincent <makokhavomondi@gmail.com>
2023-05-04 17:52:33 +0300

    Create the "CreateBot" page
    Here is where the users will be creating their bots and testing them

---
+++ src/Pages/CreateBot/CreateBot.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CreateBot.css"
+import DoYourMath from "../../Components/DoYourMath/DoYourMath";
+
+function CreateBot(){
+    return (
+        <div id="create-bot">
+            <DoYourMath />
+        </div>
+    )
+}
+
+export default CreateBot;
