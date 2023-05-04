a17277de1a7207d4b312d3384da2c7be1338a63e
Vincent <makokhavomondi@gmail.com>
2023-05-04 23:47:27 +0300

    Add header and search bar to the item list

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Search from "../../Elements/Search/Search";
 import "./ItemList.css"
 
-function ItemList(){
+function ItemList({subject}){
     return (
         <div id="item-list" className="border-primary margin-primary">
-
+            <h2 className="margin-primary">{subject}</h2>
+            <Search />
         </div>
     )
 }
