cdfee71aca86b0866182197edb9f8cf71534f8a6
Vincent <makokhavomondi@gmail.com>
2023-05-04 20:45:48 +0300

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import CreateBot from './Pages/CreateBot/CreateBot';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <CreateBot />
     </div>
   );
 }
