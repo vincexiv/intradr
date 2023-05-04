01e16fad008c61e0b8a0c85b7eb6264868df8b5a
Vincent <makokhavomondi@gmail.com>
2023-05-04 20:28:45 +0300

    Update the "DoYourMath" component styling

---
+++ src/Components/ItemList/ItemList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ItemList.css"
+
+function ItemList(){
+    return (
+        <div id="item-list" className="border-primary margin-primary">
+
+        </div>
+    )
+}
+
+export default ItemList;
