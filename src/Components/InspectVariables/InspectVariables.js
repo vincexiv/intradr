8b80dc1f2f9e1f1e884cee32a280a5157efad762
Vincent <makokhavomondi@gmail.com>
2023-05-04 20:25:20 +0300

    Add "InspectVariables" component
    will be used to show available variables and the ones currentoy on use

---
+++ src/Components/InspectVariables/InspectVariables.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./InspectVariables.css"
+
+function InspectVariables(){
+    return (
+        <div id="inspect-variables" className="border-primary margin-primary">
+
+        </div>
+    )
+}
+
+export default InspectVariables;
