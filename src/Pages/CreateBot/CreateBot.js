415e3b59a8491828df7b65dda58ca79e65d7751a
Vincent <makokhavomondi@gmail.com>
2023-05-04 20:30:18 +0300

    Update possible actions on the "CreateBot" component
    User can switch between writing trading strategy or viewing the state of the variables by clicking on the buttons provided. The buttons for backtesting and running the strategy have also been created

--- src/Pages/CreateBot/CreateBot.js
+++ src/Pages/CreateBot/CreateBot.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import "./CreateBot.css"
 import DoYourMath from "../../Components/DoYourMath/DoYourMath";
+import InspectVariables from "../../Components/InspectVariables/InspectVariables";
+import ItemList from "../../Components/ItemList/ItemList";
+import Button from "../../Elements/Button/Botton";
 
 function CreateBot(){
+    const [action, setAction] = useState(<DoYourMath />)
+
     return (
         <div id="create-bot">
-            <DoYourMath />
+            <div className="right-side-items">
+                <ItemList />
+                <ItemList />
+            </div>
+
+            <div>
+                <div>
+                    <ul className="actions">
+                        <li className="margin-primary border-primary">
+                            <Button
+                                btnStyles={{}}
+                                text="create bot"
+                                onBtnClick={()=>setAction(<DoYourMath />)} />
+                        </li>
+                        <li className="margin-primary border-primary">
+                            <Button
+                                btnStyles={{}} 
+                                text="inspect variables"
+                                onBtnClick={()=>setAction(<InspectVariables />)} />
+                        </li>
+                    </ul>
+                </div>
+
+                {action}
+
+                <div>
+                    <ul className="actions">
+                        <li className="margin-primary border-primary">
+                            <Button
+                                btnStyles={{width: "10rem"}}
+                                text="backtest"
+                                onBtnClick={()=>setAction(<DoYourMath />)} />
+                        </li>
+                        <li className="margin-primary border-primary">
+                            <Button
+                                btnStyles={{width: "10rem"}} 
+                                text="run"
+                                onBtnClick={()=>setAction(<InspectVariables />)} />
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+            <ItemList />
         </div>
     )
 }
