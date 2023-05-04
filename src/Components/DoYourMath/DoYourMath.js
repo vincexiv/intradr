6409aca055c8f3c196d05251784c15c761471c9f
Vincent <makokhavomondi@gmail.com>
2023-05-04 17:51:44 +0300

    Create the DoYourMath component

---
+++ src/Components/DoYourMath/DoYourMath.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DoYourMath.css"
+
+function DoYourMath(){
+    return (
+        <textarea id="do-your-math">
+
+        </textarea>
+    )
+}
+
+export default DoYourMath;
