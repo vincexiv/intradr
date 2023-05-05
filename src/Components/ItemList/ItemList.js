890f8fa39dc18408916539f93b5f16f43ebbf752
Vincent <makokhavomondi@gmail.com>
2023-05-05 20:38:43 +0300

    Enable tracking and untracking assets

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,12 +1,29 @@
 import React from "react";
-import Search from "../../Elements/Search/Search";
 import "./ItemList.css"
+import Search from "../../Elements/Search/Search";
+import Button from "../../Elements/Button/Botton";
+import ListItem from "../../Elements/ListItem/ListItem";
 
-function ItemList({subject}){
+function ItemList({subject, itemList=[], clickActionName="Track", clickActionFunction}){
+    const theList = itemList?.map(item => {
+        const btn = <Button 
+                        key={`btn-${item.name}`}
+                        btnStyles={{padding: "0.2rem 0.5rem 0.2rem 0.5rem", fontSize: "0.8rem"}}
+                        onBtnClick={()=>clickActionFunction(item)}
+                        text={clickActionName}/>
+
+        return <ListItem key={`list-item-${item.name}`} btnElement={btn} text={item.name}/>
+    })
+
+
     return (
         <div id="item-list" className="border-primary margin-primary">
             <h2 className="margin-primary">{subject}</h2>
             <Search />
+
+            <ul id="the-list" className="margin-primary">
+                {theList}
+            </ul>
         </div>
     )
 }
--- src/Pages/CreateBot/CreateBot.js
+++ src/Pages/CreateBot/CreateBot.js
@@ -6,13 +6,58 @@ import ItemList from "../../Components/ItemList/ItemList";
 import Button from "../../Elements/Button/Botton";
 
 function CreateBot(){
-    const [action, setAction] = useState(<DoYourMath />)
+    const [componentState, setComponentState] = useState({
+        action: <DoYourMath />,
+        untrackedAssets: [{name: "AAPL"}, {name: "MSFT"}, {name: "What"}],
+        trackedAssets: [{name: "ABC"}, {name: "DEF"}, {name: "GEH"}]
+    })
+
+    function startTracking(trackedAsset){
+        const updatedUntrackedAssets = componentState.untrackedAssets.filter(asset => asset.name !== trackedAsset.name)
+        const updatedTrackedAssets = [...componentState.trackedAssets, trackedAsset]
+
+        setComponentState(componentState => ({
+            ...componentState,
+            untrackedAssets: updatedUntrackedAssets,
+            trackedAssets: updatedTrackedAssets
+        }))
+    }
+
+    function stopTracking(untrackedAsset){
+        console.log(componentState)
+
+        const updatedTrackedAssets = componentState.trackedAssets.filter(asset => asset.name !== untrackedAsset.name)
+        const updatedUntrackedAssets = [...componentState.untrackedAssets, untrackedAsset]
+
+        setComponentState(componentState => ({
+            ...componentState,
+            untrackedAssets: updatedUntrackedAssets,
+            trackedAssets: updatedTrackedAssets
+        }))
+    }
+
+    function openAction(action){
+        if(action === 'do-your-math'){
+            setComponentState(componentState => ({...componentState, action: <DoYourMath />}))
+        }else if (action === 'inspect-variables'){
+            setComponentState(componentState => ({...componentState, action: <InspectVariables />}))
+        }
+    }
+
 
     return (
         <div id="create-bot">
             <div className="right-side-items">
-                <ItemList subject="Assets"/>
-                <ItemList subject="Tracking"/>
+                <ItemList
+                        subject="Assets"
+                        clickActionName="Track"
+                        itemList={componentState?.untrackedAssets}
+                        clickActionFunction={startTracking}/>
+                <ItemList
+                        subject="Tracking"
+                        clickActionName="Untrack"
+                        itemList={componentState?.trackedAssets}
+                        clickActionFunction={stopTracking}/>
             </div>
 
             <div>
@@ -22,24 +67,24 @@ function CreateBot(){
                             <Button
                                 btnStyles={{minWidth: "10rem"}}
                                 text="create bot"
-                                onBtnClick={()=>setAction(<DoYourMath />)} />
+                                onBtnClick={()=>openAction('do-your-math')} />
                         </li>
                         <li className="margin-primary border-primary">
                             <Button
                                 btnStyles={{minWidth: "10rem"}} 
                                 text="inspect variables"
-                                onBtnClick={()=>setAction(<InspectVariables />)} />
+                                onBtnClick={()=> openAction('inspect-variables')} />
                         </li>
                         <li className="margin-primary border-primary">
                             <Button
                                 btnStyles={{minWidth: "10rem"}} 
                                 text="set expectations"
-                                onBtnClick={()=>setAction(<InspectVariables />)} />
+                                onBtnClick={()=>openAction('do-your-math')} />
                         </li>
                     </ul>
                 </div>
 
-                {action}
+                {componentState.action}
 
                 <div>
                     <ul className="actions">
@@ -47,13 +92,13 @@ function CreateBot(){
                             <Button
                                 btnStyles={{width: "10rem"}}
                                 text="backtest"
-                                onBtnClick={()=>setAction(<DoYourMath />)} />
+                                onBtnClick={()=>openAction('do-your-math')} />
                         </li>
                         <li className="margin-primary border-primary">
                             <Button
                                 btnStyles={{width: "10rem"}} 
                                 text="start trading"
-                                onBtnClick={()=>setAction(<InspectVariables />)} />
+                                onBtnClick={()=>openAction('inspect-variables')} />
                         </li>
                     </ul>
                 </div>
