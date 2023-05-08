86894dedb864b82b613243d3f0d380d51ac4c455
Vincent <makokhavomondi@gmail.com>
2023-05-08 19:55:35 +0300

    Reduce words used to explain what needs to be done on building strategy

--- src/Components/DoYourMath/DoYourMath.js
+++ src/Components/DoYourMath/DoYourMath.js
@@ -3,13 +3,7 @@ import "./DoYourMath.css"
 
 function DoYourMath(){
     const [strategy, setStrategy] = useState(
-`
-You can write your strategy here.
-
-It is as simple as typing mathematical expressions, which will be interpreted by our system upon deploying.
-
-When you finish and are satisfied with your work, you can click "Backtest" to test your strategy and then "Run Bot" 😎
-`)
+`You can write your strategy here.`)
     function updateStrategy(e){
         setStrategy(e.target.value)
     }
