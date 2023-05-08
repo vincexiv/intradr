95c7c2f59de66013ae96513d1700eafaf0efae5b
Vincent <makokhavomondi@gmail.com>
2023-05-08 13:32:56 +0300

    Create the signal component that will hold signals

---
+++ src/Components/Signal/Signal.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import "./Signal.css";
+import SelectOption from "../../Elements/SelectOption/SelectOption";
+import Button from "../../Elements/Button/Botton";
+
+
+function Signal({constraints, updateConstraintCount, variables = ["AAPL_return"]}){
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+
+    return (
+        <div id="signal">
+            <form onSubmit={handleSubmit}>
+                <div id="objective-function" className="signal-element">
+                    <SelectOption options={["maximize", "minimize"]}/>
+                    <SelectOption options={variables}/>
+                </div>
+
+                <div>
+                    <div id="subject-to">
+                        <div>
+                            {constraints}
+                        </div>
+                        <Button btnStyles={{}}
+                                text="Add Constraint"
+                                onBtnClick={(e) => updateConstraintCount("increase")}/>
+                    </div>
+                    <input type="submit" value="Run Bot" onClick={handleSubmit}/>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Signal;
