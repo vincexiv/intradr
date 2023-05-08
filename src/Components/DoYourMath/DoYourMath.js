d802c10635e637ae652d21311798f53288334326
Vincent <makokhavomondi@gmail.com>
2023-05-08 14:07:17 +0300

    Make the DoYourMath input area a controlled form

--- src/Components/DoYourMath/DoYourMath.js
+++ src/Components/DoYourMath/DoYourMath.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./DoYourMath.css"
 
 function DoYourMath(){
-    return (
-        <textarea id="do-your-math" className="border-primary margin-primary">
+    const [strategy, setStrategy] = useState(
+`
+You can write your strategy here.
 
+It is as simple as typing mathematical expressions, which will be interpreted by our system upon deploying.
+
+When you finish and are satisfied with your work, you can click "Backtest" to test your strategy and then "Run Bot" 😎
+`)
+    function updateStrategy(e){
+        setStrategy(e.target.value)
+    }
+
+    return (
+        <textarea id="do-your-math" className="border-primary margin-primary" value={strategy} onChange={updateStrategy}>
         </textarea>
     )
 }
