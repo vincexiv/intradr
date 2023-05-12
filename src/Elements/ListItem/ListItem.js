f045ea0de93b3cb01a0dced725115587645294a4
Vincent <makokhavomondi@gmail.com>
2023-05-12 19:42:20 +0300

    Remove text wrap for list items when space is small

--- src/Elements/ListItem/ListItem.js
+++ src/Elements/ListItem/ListItem.js
@@ -5,8 +5,8 @@ function ListItem({text1, text2, btnElement}){
     return (
         <li id="list-item" className="border-primary margin-primary">
             <div id="text">
-                <p>{text1}</p>
-                <p>{text2}</p>
+                <p id="long-name">{text1}</p>
+                <p id="ticker">{text2}</p>
             </div>
             {btnElement}
         </li>
