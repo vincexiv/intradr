70c8700bd85984c0f5b19f8f0e2e13ddedcd4762
Vincent <makokhavomondi@gmail.com>
2023-05-16 16:30:13 +0300

    Empty default variables for now

--- src/Pages/CreateBot/CreateBot.js
+++ src/Pages/CreateBot/CreateBot.js
@@ -18,7 +18,7 @@ function CreateBot({market = "US", index="N/A", limit=100}){
         untrackedAssets: getUntrackedAssets(availableAssets, []),
         trackedAssets: [],
         constraintCount: 1,
-        variables: [{name: "AAPL_return", value: 0.053773}],
+        variables: [],
         bots: [{name: "xiv_bot", value: ""}],
         market: market,
         index: index,
