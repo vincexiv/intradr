81c63d1b9ed3bb0317ad2d70c4a424135aa4645f
Vincent <makokhavomondi@gmail.com>
2023-05-16 23:54:33 +0300

    Add charts component

---
+++ src/Charts/LineGraph/LineGraph.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
+function LineGraph({figureDetails}){
+  
+  const xvals = figureDetails.xvals
+
+    const ds = figureDetails?.figureData.map(data => {
+        return {
+            label: data.title,
+            ...data.lineGraphStyle,
+            data: data.yvals
+        }
+    })
+
+    const data = {
+    labels: xvals,
+    datasets: ds,
+    };
+
+  return (
+    <div>
+        <Line data={data} />
+    </div>
+  );
+};
+
+export default LineGraph;
