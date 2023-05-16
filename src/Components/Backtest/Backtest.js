a09148d3a90af05ae2ecca723669a8057b5db894
Vincent <makokhavomondi@gmail.com>
2023-05-16 16:38:34 +0300

    Create backtest component

---
+++ src/Components/Backtest/Backtest.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Backtest.css"
+
+function Backtest(){
+    return (
+        <div id="backtest" className="border-primary margin-primary create-bot-main-area">
+
+        </div>
+    )
+}
+
+export default Backtest
