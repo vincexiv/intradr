7dbc8bb08d44057ba3bd18d7e621fd688ec9aec1
Vincent <makokhavomondi@gmail.com>
2023-05-23 13:03:33 +0300

    Fix a bug with Inspect variables by converting float numbers represented in string to type "Number"

---
+++ src/Components/InspectVariables/InspectVariables.js
@@ -0,0 +1,82 @@
+import React, {useState, useEffect} from "react";
+import "./InspectVariables.css"
+import { apiHost } from "../../variables";
+import LineGraph from "../../Charts/LineGraph/LineGraph";
+
+function InspectVariables({componentState, setComponentState}){
+
+    useEffect(()=>{
+        fetch(`${apiHost}/graph_data`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                expression_array: componentState.expressionArray,
+                assets: componentState.trackedAssets?.map(ta => ta.symbol),
+                backdate_period: componentState.figureDetails.backdate_period,
+            })
+        }).then(res => {
+            if(res.ok){
+                res.json().then(data => {  
+                    setComponentState(componentState => {
+                        return {
+                            ...componentState,
+                            figureDetails: {
+                                ...componentState.figureDetails,
+                                xvals: getXvals(data),
+                                figureData: getFigureData(data)
+                            } 
+                        }
+                    })
+                })
+            }else{
+                res.json().then(error => console.warn(error))
+            }
+        })
+    }, [])
+
+    function getXvals(data){
+        for(let variableName in data.graph_data){
+            return data.graph_data[variableName].map(v => v[0])
+        }
+    }
+
+    function getFigureData(data){
+        console.log("data: ", data)
+        const result = []
+        for(const variable in data.graph_data){
+            const yVals = data.graph_data[variable].map(v => Number(v[1]))
+            const title = String(variable)
+            const lineGraphStyle = {}
+            console.log("yvals", yVals)
+            
+            result.push({yvals: yVals, title: title, lineGraphStyle: lineGraphStyle})
+        }
+
+        return result
+    }
+
+
+    return (
+        <div id="inspect-variables" className="border-primary margin-primary create-bot-main-area">
+            <div id="inspect-variables-figure">
+                <LineGraph componentState={componentState}/>
+            </div>
+            <div>
+                <ul id="variables">
+                    {
+                        componentState.variables.map(variable => {
+                            return (
+                                <li key={variable.name} className="variable-name">{variable.name}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default InspectVariables;
