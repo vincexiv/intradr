d75051e28a23377e491226fbfbde03b38979fac7
Vincent <makokhavomondi@gmail.com>
2023-05-23 13:03:02 +0300

    Move component state to App.js. Also start using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 // import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
+import { Routes, Route, useNavigate, useLocation} from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import CreateBot from './Pages/CreateBot/CreateBot';
+import { availableAssets } from './Pages/CreateBot/UtilityVariables';
+import { getUntrackedAssets } from './Pages/CreateBot/UtilityFunctions';
 
 function App() {
+  const location = useLocation()
+  const [componentState, setComponentState] = useState({
+    activePage: location.pathname || localStorage.getItem('activePage') || 'home',
+    action: "do-your-math",
+    fullAssetList: availableAssets,
+    untrackedAssets: getUntrackedAssets(availableAssets, []),
+    trackedAssets: [],
+    constraintCount: 1,
+    variables: [],
+    bots: [{name: "xiv_bot", value: ""}],
+    market: "US",
+    index: "N/A",
+    limit: 100,
+    portfolioSize: 0,
+    strategy: `You can write your strategy here.`,
+    expressionArray: [],
+    figureDetails: {
+        backdate_period: 10,
+        graph_type: "line",
+        xvals: [],
+        figureData: [{
+            yvals: [],
+            title: "",
+            lineGraphStyle: {}
+        }],
+        plotVariables: []
+    }
+  })
+
   return (
     <div className="App">
       <Navbar />
-      <CreateBot />
+      <Routes>
+          <Route path="/" element={<CreateBot componentState={componentState} setComponentState={setComponentState}/>} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/CreateBot/CreateBot.js
+++ src/Pages/CreateBot/CreateBot.js
@@ -8,40 +8,13 @@ import LeftSide from "./LeftSideComponent/LeftSide";
 import RightSide from "./RightSideComponent/RightSide";
 import MiddleSide from "./MiddleSideComponent/MiddleSide";
 import { apiHost } from "../../variables";
-import { availableAssets } from "./UtilityVariables";
 import { getUntrackedAssets } from "./UtilityFunctions";
 import Backtest from "../../Components/Backtest/Backtest";
 
-function CreateBot({market = "US", index="N/A", limit=100}){
-    const [componentState, setComponentState] = useState({
-        action: "do-your-math",
-        fullAssetList: availableAssets,
-        untrackedAssets: getUntrackedAssets(availableAssets, []),
-        trackedAssets: [],
-        constraintCount: 1,
-        variables: [],
-        bots: [{name: "xiv_bot", value: ""}],
-        market: market,
-        index: index,
-        portfolioSize: 0,
-        strategy: `You can write your strategy here.`,
-        expressionArray: [],
-        figureDetails: {
-            backdate_period: 30,
-            graph_type: "line",
-            xvals: [],
-            figureData: [{
-                yvals: [],
-                title: "",
-                lineGraphStyle: {}
-            }],
-            plotVariables: []
-        }
-    })
-
+function CreateBot({componentState, setComponentState}){
     useEffect(()=>{
         if(setComponentState){
-            fetch(`${apiHost}/assets?market=${market}&index=${index}&limit=${limit}`, {
+            fetch(`${apiHost}/assets?market=${componentState.market}&index=${componentState.index}&limit=${componentState.limit}`, {
                 method: "GET",
                 headers: {'Accept': 'application/json'},
             }).then(res => {
