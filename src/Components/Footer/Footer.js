e917d50260d131d00b834704db9dab3dfd63255f
Code-X <andrewomosh44@gmail.com>
2023-05-26 22:57:06 +0300

    feat: add footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
+    const date = new Date()
+    const today = date.getFullYear()
     return ( 
         <>
-            Footer section here...
+            <div className="bg-purple-500 text-center md:text-2xl text-xl py-6 md:mt-16 mt-8 text-white font-semibold">
+                <p>All Rights Reserved © 
+                    <span className="mx-1">{today}</span>|
+                    <Link to="/" className="hover:text-[#FE741A] ml-1">Intradr</Link>
+                </p>
+            </div>
         </>
      );
 }
