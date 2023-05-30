2b41acb547015d277ef433b7dfe8e9a044259c3a
Vincent <makokhavomondi@gmail.com>
2023-05-30 12:19:09 +0300

    Add items to the navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,14 @@ import "./Navbar.css"
 function Navbar(){
     return (
         <div id="navbar">
-
+            <div className="content">
+                <h2>Intradr</h2>
+                <ul>
+                    <li><h3>Demo</h3></li>
+                    <li><h3>About Us</h3></li>
+                    <li><h3>Contact</h3></li>
+                </ul>
+            </div>
         </div>
     )
 }
